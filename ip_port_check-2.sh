ab45e4ca90e76a3a262d8832715b8ce646ac4206
mengyalei <33970561+mengyalei@users.noreply.github.com>
2022-02-28 16:45:31 +0800

    Update ip_port_check-2.sh

--- ip_port_check-2.sh
+++ ip_port_check-2.sh
@@ -23,15 +23,15 @@ do
 		
     else
 		
-		    newport=`expr $port + 1`
+	newport=`expr $port + 1`
         echo $(date +"%y-%m-%d %H:%M:%S") "$ip:$port Fail! " >> telnet_fail.txt
-		    echo "$ip|$newport|" >> telnet_checked.txt
+	echo "$ip|$newport|" >> telnet_checked.txt
 		
-		    if [ "$ip" = "18.183.*.*" ]; then
-		      update_sql="update ${TABLENAME} set server='***url.com;$newport;2;ws;;path=/hls/cctv5phd.m3u8|host=***url.com' where id =5"
-		      mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${update_sql}"
-		    fi			
+	if [ "$ip" = "18.183.*.*" ]; then
+		update_sql="update ${TABLENAME} set server='***url.com;$newport;2;ws;;path=/hls/cctv5phd.m3u8|host=***url.com' where id =5"
+		mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${update_sql}"
+	fi			
     fi
 done
-	  mv  telnet_checked.txt  iplist.txt
-    echo "telnet IP_port checked over!"
+	mv  telnet_checked.txt  iplist.txt
+    	echo "telnet IP_port checked over!"
